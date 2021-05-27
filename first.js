93788fce0dfc87f93e8fc793bce3039eccce827f
Jitendra Sharma <jitendra0661@gmail.com>
2021-05-28 01:59:15 +0530

    initial commit

---
+++ first.js
@@ -0,0 +1,26 @@
+let x=`khbskajldnk
+jshbxkjk
+
+
+
+
+
+hjsxckl`;
+// x=x.split("\n")
+// console.log(x)
+// let y=[]
+// let flag=false
+// for(let i=0;i<x.length;i++){
+//     if(x[i]===''){
+//         if(flag===true){
+//             continue;
+//         }else{
+//             y.push(x[i])
+//             flag=true
+//         }
+//     }else{
+//         y.push(x[i])
+//         flag=false
+//     }
+// }
+// console.log(y)
